b8ab3c0bb9dceadc1f3ed1816f276ab2cdd72ff9
Amirulislamsohan <wwwsmsohanfake@gmail.com>
2023-06-16 11:38:23 +0600

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Component/Home/Home.jsx';
+import Header from './Component/Header/Header.jsx';
+import Layout from './Component/Layout/Layout.jsx';
+import OrderReview from './Component/OrderReview/OrderReview.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout></Layout>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+        loader: () => fetch('tshirts.json')
+      },
+      {
+        path: 'review',
+        element: <OrderReview></OrderReview>
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
